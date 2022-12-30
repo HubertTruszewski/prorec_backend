eb7d920c03651b44cd059f5c69f35c1565adcd7b
Hubert Truszewski <hubitru@gmail.com>
2022-12-30 12:26:48 +0100

    Add endpoint for all test cases for challenge

--- src/main/java/pl/edu/pw/elka/pis05/prorec/challenge/repository/TestCaseRepository.java
+++ src/main/java/pl/edu/pw/elka/pis05/prorec/challenge/repository/TestCaseRepository.java
@@ -1,8 +1,12 @@
 package pl.edu.pw.elka.pis05.prorec.challenge.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import pl.edu.pw.elka.pis05.prorec.challenge.model.Challenge;
 import pl.edu.pw.elka.pis05.prorec.challenge.model.TestCase;
 
 public interface TestCaseRepository extends JpaRepository<TestCase, Long> {
+    List<TestCase> findAllByChallenge(Challenge challenge);
 }
