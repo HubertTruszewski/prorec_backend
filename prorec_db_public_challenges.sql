cce5a91a5a8ec952574f63f52296633b3fe07f8b
Hubert Truszewski <hubitru@gmail.com>
2023-01-08 16:37:54 +0100

    Add sample tasks

---
+++ prorec_db_public_challenges.sql
@@ -0,0 +1,18 @@
+insert into public.challenges (challenge_id, name, description, code_snippet, example_test_cases, language, type) values (5, 'Prime numbers', 'Please write a function to check if a provided number is a prime number.
+A prime number (or a prime) is a natural number greater than 1 that is not a product of two smaller natural numbers. (From Wikipedia)', 'const isPrime = (n) => {
+    // your code goes here
+    return true;
+}', 'isPrime(2) === true
+isPrime(1) === false
+isPrime(14) === false', 1, 0);
+insert into public.challenges (challenge_id, name, description, code_snippet, example_test_cases, language, type) values (6, 'Factorial', 'Please write a function to calculate a factorial of a given number.
+In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n.  (From Wikipedia)', 'def factorial(n):
+    # your code goes here
+    return 1', 'factorial(2) == 2
+factorial(4) == 24', 0, 0);
+insert into public.challenges (challenge_id, name, description, code_snippet, example_test_cases, language, type) values (7, 'Fibonacci number', 'Please write a function which returns the nth element of the Fibonacci number.
+Fibbonacci number can be calculated as F(n) = F(n-1) + F(n-2), where F(1)=1, F(2)=1.', 'const fibonacci = (n) => {
+    // your code goes here
+    return 1;
+}', 'fibonacci(1) == 1
+fibonacci(4) == 3', 1, 0);
